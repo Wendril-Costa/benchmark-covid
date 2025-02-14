a0438912fccd776eb23539d5a9cd5f751dc3907b
wendril <wendrild.c.n@gmail.com>
2025-02-13 23:57:31 -0300

    futura mudança de idioma

---
+++ src/main/java/com/wendril/application/utils/Titles.java
@@ -0,0 +1,7 @@
+package com.wendril.application.utils;
+
+public final class Titles {
+    public static final String TITULO_APLICATION = "Benchmark Covid";
+    public static final String TITULO_REGISTER = "Register";
+    public static final String TITULO_LOGIN = "Login";
+}
