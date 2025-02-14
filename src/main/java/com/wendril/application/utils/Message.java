8bc8561a28bc36768d49ae0cd4421122dff151d1
wendril <wendrild.c.n@gmail.com>
2025-02-13 23:57:19 -0300

    Mensagem pra futura mudança de idioma

---
+++ src/main/java/com/wendril/application/utils/Message.java
@@ -0,0 +1,7 @@
+package com.wendril.application.utils;
+
+public final class Message {
+
+    public static final String ERRO_TITLE_LOGIN = "Erro de login";
+    public static final String ERRO_LOGIN_USERNAME_PASSWORD = "Usuário ou senha incorretos. Por favor, tente novamente.";
+}
